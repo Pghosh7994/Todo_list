67fc4f4ecdccf1a861e1c707b053f4ea5f91d38d
Pghosh7994 <priyankapinkighosh@gmail.com>
2022-03-22 08:49:41 +0600

    todo-list added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Todo from './Components/Todo/Todo';
 
 function App() {
   return (
     <div className="App">
-
+      <Todo></Todo>
     </div>
   );
 }
---
+++ src/Components/Todo/Todo.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { addTodo, deleteTodo, removeTodo } from '../../Actions/action';
+import "./todo.css";
+
+const Todo = () => {
+
+    const [inputData, setInputData] = useState('');
+    const list = useSelector((state) => state.todoReducers.list);
+    const dispatch = useDispatch();
+    return (
+        <>
+            <div className="main-div">
+                <div className="child-div">
+                    <figure>
+                        <figcaption>Add Your List Here</figcaption>
+                    </figure>
+                    <div>
+                        <input className="addItems" type="text" placeholder="Add Items"
+                            value={inputData}
+                            onChange={(event) => setInputData(event.target.value)}
+                        />
+                        <button className="todo-button" onClick={() => dispatch(addTodo(inputData),
+                            setInputData(''))}>Add</button>
+
+                    </div>
+
+                    <div className="showItems">
+                        {
+                            list.map((elem) => {
+                                return (
+                                    <div className="eachItem" key={elem.id}>
+                                        <h3>{elem.data}</h3>
+                                        <div className="todo-btn">
+                                            <i className="far fa-trash-alt add-btn" title="Delete Item" onClick={() => dispatch(deleteTodo(elem.id))}></i>
+                                        </div>
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
+                    <div className="showItems">
+                        <button className="todo-button padding" data-sm-link-text="remove All"
+                            onClick={() => dispatch(removeTodo())}>
+                            <span>Check List</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Todo;
